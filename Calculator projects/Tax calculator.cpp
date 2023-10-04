b610f4bc698cfe2ea5b7529e0c22b81e9f9cd4cc
James McCann <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-10-04 13:28:20 +0800

    Update Tax calculator.cpp

--- Calculator projects/Tax calculator.cpp
+++ Calculator projects/Tax calculator.cpp
@@ -1,7 +1,4 @@
-Azure labs Password: initial1stPw
 
-
---- Tax calculator ---
 
 int main () {
 
@@ -17,46 +14,6 @@ var_total = var_sales * (var_county_tax + var_tax);
 var_total = var_total - var_sales;
 
 cout "the total is after all tax is: " << var_total;
-
-return 0;
-}
-
-// Working out
-95,000 * 0.04 = 3800
-95,000 * 0.02 = 1900
-total tax = 5700
-
-$95,000 - $5700 = $89,300
-
-
-
-
---- Fahrenheit to celcius conversion ---
-
-int main(){
-
-cout << "Enter a temperature in Fahrenheit: ";
-int fahrenheit;
-cin >> fahrenheit;
-double celsius = (fahrenheit - 32) * 0.5556
-
-cout << "The temperature is 'fahrenheit' in celsius";
-
-return 0;
-}
-
-
---- Area of a circle calculator ---
-
-int main(){
-
-cout << "Enter the radius of the circle: ";
-double radius;
-cin >> radius;
-
-const double pi = 3.14;
-double area = pi * pow(radius, 2); //area = pi * r^2
-cout << area;
 
 return 0;
 }
